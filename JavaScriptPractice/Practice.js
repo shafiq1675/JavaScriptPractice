b8f51e89c87cdd2a914b9c141c3d25202074fa83
Md. Shafiqul Islam <islam.mdshafiq@gmail.com>
2019-02-16 17:02:52 +0600

    Add project files.

---
+++ JavaScriptPractice/Practice.js
@@ -0,0 +1,19 @@
+﻿
+
+function Helper() {
+
+}
+var name = "khan";
+Helper.AddTwoNumber = function (firstNumber, secondNUmber) {
+    return firstNumber + secondNUmber;
+};
+console.log(name);
+console.log(Helper.AddTwoNumber(5, 10));
+
+var arr = ["khan", "hasib", "shafiq"];
+console.log(arr.length);
+arr.push("ask");
+console.log(arr.length);
+console.log(arr[3]);
+arr.splice(1, 1, "khan");
+console.log(arr);
