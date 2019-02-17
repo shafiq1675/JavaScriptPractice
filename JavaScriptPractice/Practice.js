248ece1b4e742479da2ba8e8d7e99030ec68ddb0
Md. Shafiqul Islam <islam.mdshafiq@gmail.com>
2019-02-17 15:40:01 +0600

    last update

--- JavaScriptPractice/Practice.js
+++ JavaScriptPractice/Practice.js
@@ -2,14 +2,25 @@
 function Helper() {
 
 }
+
+console.log('================variable=================');
+
 let fatherName = "Omar";
 var name = "khan";
+
+console.log(name);
+console.log(fatherName);
+
+console.log('================function=================');
+
 Helper.AddTwoNumber = function (firstNumber, secondNUmber) {
     return firstNumber + secondNUmber;
 };
-console.log(name);
-console.log(fatherName);
+
 console.log(Helper.AddTwoNumber(5, 10));
+
+
+console.log('================array=================');
 
 var arr = ["khan", "hasib", "shafiq"];
 console.log(arr.length);
@@ -19,13 +30,34 @@ console.log(arr[3]);
 arr.splice(1, 1, "khan");
 console.log(arr);
 
+console.log('================for loop=================');
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]} your position ${i + 1}`);
+}
+console.log('================for each loop=================');
+arr.forEach(function (name, index) {
+    console.log(`${name} your position ${index + 1}`);
+})
+
+console.log('================works with object=================');
+
 let myToDo = {
     mettings: 0,
     mettingDone: 0,
-    addMetting: function (todo, metting) {
-        return todo.mettings += metting;
+    addMetting: function (todo, meet) {
+        return todo.mettings += meet;
     },
+    meetDone: function (todo, met) {
+        return todo.mettingDone -= met;
+    },
+    summeryCal: function (todo) {
+        return todo.mettings + todo.mettingDone;
+    }
 }
 
-myToDo.addMetting(myToDo,5);
-console.log(myToDo.mettings);
+myToDo.addMetting(myToDo, 5);
+myToDo.meetDone(myToDo, 4);
+
+console.log(`${myToDo.summeryCal(myToDo)} meeting left.`)
+
