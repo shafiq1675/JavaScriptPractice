95bcfccbed545c9b6f45a28447fc64db4d162999
Md. Shafiqul Islam <islam.mdshafiq@gmail.com>
2019-02-17 15:47:41 +0600

    last update

--- JavaScriptPractice/Practice.js
+++ JavaScriptPractice/Practice.js
@@ -35,7 +35,9 @@ console.log('================for loop=================');
 for (let i = 0; i < arr.length; i++) {
     console.log(`${arr[i]} your position ${i + 1}`);
 }
+
 console.log('================for each loop=================');
+
 arr.forEach(function (name, index) {
     console.log(`${name} your position ${index + 1}`);
 })
@@ -43,20 +45,20 @@ arr.forEach(function (name, index) {
 console.log('================works with object=================');
 
 let myToDo = {
-    mettings: 0,
-    mettingDone: 0,
-    addMetting: function (todo, meet) {
-        return todo.mettings += meet;
+    meetings: 0,
+    meetingDone: 0,
+    addMeeting: function (todo, meet) {
+        return todo.meetings += meet;
     },
     meetDone: function (todo, met) {
-        return todo.mettingDone -= met;
+        return todo.meetingDone -= met;
     },
     summeryCal: function (todo) {
-        return todo.mettings + todo.mettingDone;
+        return todo.meetings + todo.meetingDone;
     }
 }
 
-myToDo.addMetting(myToDo, 5);
+myToDo.addMeeting(myToDo, 5);
 myToDo.meetDone(myToDo, 4);
 
 console.log(`${myToDo.summeryCal(myToDo)} meeting left.`)
